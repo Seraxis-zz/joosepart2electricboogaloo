289513603ca9c957363875562dbe745b7e2a280b
“FCraig Massie <22207400M@student.gla.ac.uk>
2017-02-27 15:18:14 +0000

    Part 3 and 4 folders added

---
+++ Part3/birdsim1/src/main/java/gla/cs/joose/coursework/birdsim1/App.java
@@ -0,0 +1,13 @@
+package gla.cs.joose.coursework.birdsim1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
